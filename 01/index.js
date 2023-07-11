4dbee31449373f5ae9f700ea30bd85297a2de626
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-11 20:20:03 -0300

    Ex01 - Cadastro de Usuário

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Rua dos Bobos",
+    numero: "0",
+    complemento: "Bloco A",
+    bairro: "Copacabana",
+    cep: "00000-000",
+    cidade: "Rio de Janeiro",
+    estado: "RJ",
+    pais: "Brasil"
+};
+
+const usuario = {
+    nome: "Karla",
+    email: "meuemail@gmail.com",
+    telefone: "(21) 9 9999-9999",
+    data_nascimento: "12/01/1994",
+    endereco: endereco
+};
+
+console.log(usuario);
