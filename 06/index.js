82d922137fbbea1477e33904886174eebaa8246e
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-11 21:06:22 -0300

    Ex06 - Separando Jovens de Adultos

---
+++ 06/index.js
@@ -0,0 +1,30 @@
+const usuarios = [
+    {
+        nome: "João",
+        idade: 25,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+    },
+    {
+        nome: "Beatriz",
+        idade: 15,
+    },
+    {
+        nome: "Carlos",
+        idade: 16,
+    },
+    {
+        nome: "Antonio",
+        idade: 32,
+    },
+];
+
+const jovens = [];
+const adultos = [];
+for (let usuario of usuarios){
+    (usuario.idade < 18) ? jovens.push(usuario) : adultos.push(usuario);
+}
+console.log(jovens);
+console.log(adultos);
